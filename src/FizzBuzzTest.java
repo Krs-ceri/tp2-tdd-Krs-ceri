7647440fecc5608f024fcfd8a2bc9cfe07be1fa6
Nizar Rezaigui <nizar.rezaigui@alumni.univ-avignon.fr>
2018-11-26 15:35:30 +0100

    first

---
+++ src/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package src;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void shouldReturnUnWhen1() {
+		fail("Not yet implemented");
+	}
+
+}
